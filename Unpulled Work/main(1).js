6d9a3fc34180c8d5a6cecbba52ad9a7018328abc
Noah Brumfield <electavire@gmail.com>
2020-05-06 12:23:13 -0700

    Add files via upload

---
+++ Unpulled Work/main(1).js
@@ -0,0 +1,152 @@
+const electron = require('electron')
+const app = electron.app
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+const ProjectWindow = require("./projectWindow.js").ProjectWindow;
+const DocumentationWindow = require("./documentationWindow.js").DocumentationWindow;
+const AboutWindow = require("./aboutWindow.js").AboutWindow;
+const appmenus = require('./appmenus.js');
+const forceQuitDetect = require('./forceQuitDetect');
+const Inklecate = require("./inklecate.js").Inklecate;
+
+function inkJSNeedsUpdating() {
+    return false;
+    // dialog.showMessageBox({
+    //   type: 'error',
+    //   buttons: ['Okay'],
+    //   title: 'Export for web unavailable',
+    //   message: "Sorry, export for web is currently disabled, until inkjs is updated to support the latest version of ink. You can download a previous version of Inky that supports inkjs and use that instead, although some of the latest features of ink may be missing."
+    // });
+    // return true;
+}
+
+app.on('will-finish-launching', function () {
+    app.on("open-file", function (event, path) {
+        ProjectWindow.open(path);
+        event.preventDefault();
+    });
+
+});
+
+let isQuitting = false;
+
+app.on('before-quit', function () {
+    // We need this to differentiate between pressing quit (which should quit) or closing all windows
+    // (which leaves the app open)
+    isQuitting = true;
+});
+
+ipc.on("project-cancelled-close", (event) => {
+    isQuitting = false;
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+
+    app.on('window-all-closed', function () {
+        if (process.platform != 'darwin' || isQuitting) {
+            app.quit();
+        }
+    });
+
+    appmenus.setupMenus({
+        new: () => {
+            ProjectWindow.createEmpty();
+        },
+        newInclude: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.newInclude();
+        },
+        open: () => {
+            ProjectWindow.open();
+        },
+        save: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.save();
+        },
+        exportJson: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.exportJson();
+        },
+        exportForWeb: () => {
+            if( inkJSNeedsUpdating() ) return;
+            var win = ProjectWindow.focused();
+            if (win) win.exportForWeb();
+        },
+        exportJSOnly: () => {
+            if( inkJSNeedsUpdating() ) return;
+            var win = ProjectWindow.focused();
+            if (win) win.exportJSOnly();
+        },
+        toggleTags: (item, focusedWindow, event) => {
+            focusedWindow.webContents.send("set-tags-visible", item.checked);
+        },
+        nextIssue: (item, focusedWindow) => {
+            focusedWindow.webContents.send("next-issue");
+        },
+        gotoAnything: (item, focusedWindow) => {
+            focusedWindow.webContents.send("goto-anything");
+        },
+        addWatchExpression: (item, focusedWindow) => {
+            focusedWindow.webContents.send("add-watch-expression");
+        },
+        showDocs: () => {
+            DocumentationWindow.openDocumentation();
+        },
+        showAbout: () => {
+            AboutWindow.showAboutWindow();
+        },
+        countWords: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.countWords();
+        },
+		countKnots: () => {
+            var win = ProjectWindow.focused();
+            if (win) win.countKnots();
+        },
+        zoomIn: () => {
+          var win = ProjectWindow.focused();
+          if (win != null) {
+            win.zoom(2);
+          }
+        },
+        zoomOut: () => {
+          var win = ProjectWindow.focused();
+          if (win != null) {
+            win.zoom(-2);
+          }
+        },
+        insertSnippet: (focussedWindow, snippet) => {
+            if( focussedWindow )
+                focussedWindow.webContents.send('insertSnippet', snippet);
+        }
+    });
+
+    let openedSpecificFile = false;
+    if (process.platform == "win32" && process.argv.length > 1) {
+        for (let i = 1; i < process.argv.length; i++) {
+            var arg = process.argv[i];
+            if (arg.indexOf(".ink") == arg.length - 4) {
+                var fileToOpen = process.argv[1];
+                ProjectWindow.open(fileToOpen);
+                openedSpecificFile = true;
+                break;
+            }
+        }
+    }
+    if (!openedSpecificFile) {
+        var w = ProjectWindow.createEmpty();
+    }
+
+    // Debug
+    //w.openDevTools();
+});
+
+function finalQuit() {
+    Inklecate.killSessions();
+}
+
+forceQuitDetect.onForceQuit(finalQuit);
+electron.app.on("will-quit", finalQuit);
---
+++ Unpulled Work/projectWindow(1).js
@@ -0,0 +1,160 @@
+const electron = require('electron');
+const ipc = electron.ipcMain;
+const dialog = electron.dialog;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const Inklecate = require("./inklecate.js").Inklecate;
+const Menu = electron.Menu;
+
+const electronWindowOptions = {
+  width: 1300,
+  height: 730,
+  minWidth: 350,
+  minHeight: 250,
+  titleBarStyle: 'hidden',
+  title: "Inky"
+};
+
+var windows = [];
+
+function ProjectWindow(filePath) {
+    const getThemeFromMenu = () => Menu.getApplicationMenu().items.find(
+        e => e.label.toLowerCase() === 'view'
+    ).submenu.items.find(
+        e => e.label.toLowerCase() === 'theme'
+    ).submenu.items.find(
+        e => e.checked
+    ).label.toLowerCase();
+
+    this.browserWindow = new BrowserWindow(electronWindowOptions);
+    this.browserWindow.loadURL("file://" + __dirname + "/../renderer/index.html");
+    this.browserWindow.setSheetOffset(49);
+
+    this.safeToClose = false;
+
+    if( filePath ) {
+        this.browserWindow.webContents.on('dom-ready', () => {
+            this.browserWindow.setRepresentedFilename(filePath);
+            this.browserWindow.webContents.send('set-project-main-ink-filepath', filePath);
+        });
+    }
+
+    windows.push(this);
+
+    this.browserWindow.on("close", (event) => {
+        if( !this.safeToClose ) {
+            event.preventDefault();
+            this.tryClose();
+        }
+    })
+
+    this.browserWindow.on("closed", () => {
+        var idx = windows.indexOf(this);
+        if( idx != -1 )
+            windows.splice(idx, 1);
+    });
+
+    this.browserWindow.webContents.on('dom-ready', () => {
+        this.browserWindow.send("change-theme", getThemeFromMenu());
+    });
+}
+
+ProjectWindow.prototype.newInclude = function() {
+    this.browserWindow.webContents.send('project-new-include');
+}
+
+ProjectWindow.prototype.save = function() {
+    this.browserWindow.webContents.send('project-save');
+}
+
+ProjectWindow.prototype.exportJson = function() {
+    this.browserWindow.webContents.send('project-export');
+}
+
+ProjectWindow.prototype.exportForWeb = function() {
+    this.browserWindow.webContents.send('project-export-for-web');
+}
+
+ProjectWindow.prototype.exportJSOnly = function() {
+    this.browserWindow.webContents.send('project-export-js-only');
+}
+
+ProjectWindow.prototype.tryClose = function() {
+    this.browserWindow.webContents.send('project-tryClose');
+}
+
+ProjectWindow.prototype.countWords = function() {
+    this.browserWindow.webContents.send('project-count-words');
+}
+
+ProjectWindow.prototype.countKnots = function() {
+    this.browserWindow.webContents.send('project-count-knots');
+}
+
+ProjectWindow.prototype.finalClose = function() {
+    this.safeToClose = true;
+    Inklecate.killSessions(this.browserWindow);
+    this.browserWindow.close();
+}
+
+ProjectWindow.prototype.openDevTools = function() {
+    this.browserWindow.webContents.openDevTools();
+}
+
+ProjectWindow.prototype.zoom = function(amount) {
+    this.browserWindow.webContents.send('zoom', amount);
+}
+
+ProjectWindow.all = () => windows;
+
+ProjectWindow.createEmpty = function() {
+    return new ProjectWindow();
+}
+
+ProjectWindow.focused = function() {
+    var browWin = BrowserWindow.getFocusedWindow();
+    if( browWin )
+        return ProjectWindow.withWebContents(browWin.webContents);
+    else
+        return null;
+}
+
+
+ProjectWindow.withWebContents = function(webContents) {
+    if( !webContents )
+        return null;
+
+    for(var i=0; i<windows.length; i++) {
+        if( windows[i].browserWindow.webContents === webContents )
+            return windows[i];
+    }
+    return null;
+}
+
+ProjectWindow.open = function(filePath) {
+    if( !filePath ) {
+        var multiSelectPaths = dialog.showOpenDialog({
+            title: "Open main ink file",
+            properties: ['openFile'],
+            filters: [
+                { name: 'Ink files', extensions: ['ink'] }
+            ]
+        });
+
+        if( multiSelectPaths && multiSelectPaths.length > 0 )
+            filePath = multiSelectPaths[0];
+    }
+
+    // TODO: Could check whether the filepath is relative to any of our
+    // existing open projects, and switch to that window?
+
+    if( filePath)
+        return new ProjectWindow(filePath);
+}
+
+ipc.on("project-final-close", (event) => {
+    var win = ProjectWindow.withWebContents(event.sender);
+    win.finalClose();
+});
+
+exports.ProjectWindow = ProjectWindow;
